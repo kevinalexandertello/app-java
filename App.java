5729a41911464eff9a58799e6c5fd69648c6d263
kevinalexandertello <gaston_alexander@hotmail.es>
2021-01-29 22:57:53 -0300

    newAppJava

--- App.java
+++ App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello my name is Kevin Tello and Federico Gomez");
+        System.out.println("Hello my name is Kevin Tello and Federico Gomez helped me to nnderstand in a better way how it works Github");
     }
 }
